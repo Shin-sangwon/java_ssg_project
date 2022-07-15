f80c57eb4cc2174066766e24db61e939ca423bac
Shin-sangwon <dnjsml10@gmail.com>
2022-07-15 13:24:56 +0900

    step3

--- src/main/java/com/ll/ssg/App.java
+++ src/main/java/com/ll/ssg/App.java
@@ -7,13 +7,24 @@ public class App {
     Scanner sc;
     void run(){
         System.out.println("== 명언 SSG ==");
+
         sc = new Scanner(System.in);
-        String cmd = sc.nextLine();
+
 
         Loop1:
         while(true){
+            System.out.println("명령을 입력하세요");
+            System.out.print("명령) : ");
+            String cmd = sc.nextLine();
             switch(cmd){
                 case "종료" : break Loop1;
+                case "등록" :
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+                    System.out.println("1번 명언이 등록되었습니다.");
+                    break;
             }
         }
 
